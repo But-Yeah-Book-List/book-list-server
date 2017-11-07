18369b348ef4832af14b95ea6f8cbd50ccf5771f
Robert Reed <robert.mc.reed@gmail.com>
2017-11-07 10:39:53 -0800

    initial server setup

--- server.js
+++ server.js
@@ -3,3 +3,19 @@
 const express = require('express');
 const cors = require('cors');
 const pg = require('pg');
+
+const app = express();
+const PORT = process.env.PORT;
+const CLIENT_URL = process.env.CLIENT_URL;
+
+// const client = new pg.Client(process.env.DATABASE_URL);
+// client.connect();
+// client.on('error', err => console.error(err));
+
+app.use(cors());
+
+app.get('/test', (req, res) => res.send('hello boogs'));
+
+app.get('*', (req, res) => res.redirect(CLIENT_URL));
+
+app.listen(PORT, () => console.log(`listening on port: ${PORT}`));
