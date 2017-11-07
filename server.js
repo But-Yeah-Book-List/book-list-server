94c575cf2f9c555f7bc7d2643e31107b863a5d9e
Robert Reed <robert.mc.reed@gmail.com>
2017-11-07 11:02:21 -0800

    uncommented pg client lines

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const app = express();
 const PORT = process.env.PORT;
 const CLIENT_URL = process.env.CLIENT_URL;
 
-// const client = new pg.Client(process.env.DATABASE_URL);
-// client.connect();
-// client.on('error', err => console.error(err));
+const client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
+client.on('error', err => console.error(err));
 
 app.use(cors());
 
